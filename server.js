ba9b8797eb1a54ba55680fc587fc881507c5e196
steve <ourusteve13@gmail.com>
2025-05-25 13:48:34 +0300

    cron job added and ratelimiter modified

--- server.js
+++ server.js
@@ -4,14 +4,21 @@ import cors from 'cors'
 import transactionRoute from "./routes/transactionRoute.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import { initDB } from "./config/db.js";
+import job from './config/cron.js'
 
 const app = express();
+
+if(process.env.NODE_ENV === "production") job.start
 const port = process.env.PORT || 5001;
 
 app.use(rateLimiter)
 app.use(express.json())
 app.use(cors())
 app.use("/api/transactions", transactionRoute)
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({status: "ok"})
+})
 
 initDB().then(() => {
   app.listen(port, () => {
